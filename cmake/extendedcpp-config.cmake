231a35ac88500227ce08e9d094c9b87bb23160a0
KorablikDimak <korablikdimak@gmail.com>
2024-10-12 00:50:48 +0300

    fix installing end export

---
+++ cmake/extendedcpp-config.cmake
@@ -0,0 +1,2 @@
+include(${CMAKE_CURRENT_LIST_DIR}/extendedcpp-targets.cmake)
+message(-- Found ExtendedCpp (version ${${CMAKE_FIND_PACKAGE_NAME}_VERSION}))
--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,36 +1,36 @@
 function(set_interface_include_directories)
     foreach(target ${ARGN})
         target_include_directories(${target} INTERFACE
-                "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>"
-                "$<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/include>"
-                "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
+                $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
+                $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/include>
+                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
     endforeach()
 endfunction()
 
 function(set_public_include_directories)
     foreach(target ${ARGN})
         target_include_directories(${target} PUBLIC
-                "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>"
-                "$<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/include>"
-                "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
+                $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
+                $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/include>
+                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
     endforeach()
 endfunction()
 
 function(install_projects)
     install(TARGETS ${ARGN}
-            EXPORT "${PROJECT_NAME}-targets"
-            RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}"
-            ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-            LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
-            INCLUDES DESTINATION include)
+            EXPORT ${PROJECT_NAME}-targets
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
-    if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    if(CMAKE_CXX_COMPILER_ID MATCHES MSVC)
         foreach(target ${ARGN})
             get_target_property(target_pdb_name ${target} COMPILE_PDB_NAME)
             get_target_property(target_pdb_name_debug ${target} COMPILE_PDB_NAME_DEBUG)
             get_target_property(target_pdb_output_directory ${target} PDB_OUTPUT_DIRECTORY)
             install(FILES
-                    "${target_pdb_output_directory}/\${CMAKE_INSTALL_CONFIG_NAME}/$<$<CONFIG:Debug>:${target_pdb_name_debug}>$<$<NOT:$<CONFIG:Debug>>:${target_pdb_name}>.pdb"
+                    ${target_pdb_output_directory}/\${CMAKE_INSTALL_CONFIG_NAME}/$<$<CONFIG:Debug>:${target_pdb_name_debug}>$<$<NOT:$<CONFIG:Debug>>:${target_pdb_name}>.pdb
                     DESTINATION ${CMAKE_INSTALL_LIBDIR}
                     OPTIONAL)
         endforeach()
