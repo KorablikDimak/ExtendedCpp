8d1cccbbfb369b03a7860a04aa4d5f25eb770e70
KorablikDimak <korablikdimak@gmail.com>
2025-03-16 04:27:43 +0300

    fix reflection

--- src/Reflection/Assembly.cpp
+++ src/Reflection/Assembly.cpp
@@ -1,20 +1,29 @@
 #include <ExtendedCpp/Reflection/Assembly.h>
 
-std::map<std::type_index, ExtendedCpp::Reflection::TypeInfo> ExtendedCpp::Reflection::Assembly::_typeList;
-std::mutex ExtendedCpp::Reflection::Assembly::_listMutex;
+std::map<std::type_index, ExtendedCpp::Reflection::TypeInfo>& ExtendedCpp::Reflection::Assembly::TypeList() noexcept
+{
+    static std::map<std::type_index, TypeInfo> typeList;
+    return typeList;
+}
+
+std::mutex& ExtendedCpp::Reflection::Assembly::ListMutex() noexcept
+{
+    static std::mutex listMutex;
+    return listMutex;
+}
 
 void ExtendedCpp::Reflection::Assembly::AddType(const TypeInfo& typeInfo) noexcept
 {
-    std::lock_guard<std::mutex> guard(_listMutex);
-    _typeList.insert(std::make_pair(typeInfo.TypeIndex(), typeInfo));
+    std::lock_guard<std::mutex> guard(ListMutex());
+    TypeList().insert(std::make_pair(typeInfo.TypeIndex(), typeInfo));
 }
 
 std::vector<ExtendedCpp::Reflection::TypeInfo> ExtendedCpp::Reflection::Assembly::GetTypes() noexcept
 {
-    std::lock_guard<std::mutex> guard(_listMutex);
+    std::lock_guard<std::mutex> guard(ListMutex());
     std::vector<TypeInfo> types;
-    types.reserve(_typeList.size());
-    for (const auto& type : _typeList)
+    types.reserve(TypeList().size());
+    for (const auto& type : TypeList())
         types.push_back(type.second);
     return types;
 }
